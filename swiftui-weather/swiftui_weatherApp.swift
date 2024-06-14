22513b8f17f68033d7af8d12737b41b1600089f9
Chris Salvador <csalvador58@gmail.com>
2024-06-13 22:02:17 -0700

    Initial Commit

---
+++ swiftui-weather/swiftui_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_weatherApp.swift
+//  swiftui-weather
+//
+//  Created by Salvador on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
